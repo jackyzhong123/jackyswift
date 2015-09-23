3a58d216e2aae503b7d5bea07eb13a947626ef92
jackyzhong123 <jackyecnu@126.com>
2015-09-23 14:23:00 +0800

    Initial Commit

---
+++ JackySwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JackySwift
+//
+//  Created by Sky on 15/9/23.
+//  Copyright © 2015年 HuoDongOrganizer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
